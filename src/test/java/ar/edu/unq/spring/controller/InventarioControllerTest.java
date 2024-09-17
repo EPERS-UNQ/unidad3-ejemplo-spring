c4000b137c6924ce39a8ee45a3f639899b1a3073
Valentin Ferreyra <valen.ferreyra@outlook.com>
2024-09-17 01:39:38 -0300

    controller test wip

---
+++ src/test/java/ar/edu/unq/spring/controller/InventarioControllerTest.java
@@ -0,0 +1,8 @@
+package ar.edu.unq.spring.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+// Con @ContextConfiguration le indicamos que configuraciones queremos que levante
+public class InventarioControllerTest {
+}
